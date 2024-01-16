75e1ab5af4732a7c90b96a2823d8dd2aac64a527
Jobayrul Hasan <jobayerdpi11@gmail.com>
2024-01-16 22:58:07 +0600

    Add project files.

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using RoleBaseApplication.Models;
+
+namespace RoleBaseApplication.Services
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        { 
+
+        }
+    }
+}
